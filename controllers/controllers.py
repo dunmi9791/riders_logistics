1cd7ae27221e396db3d13880c20b1eeff0878ede
Dunmi <dominic.anyanna@gmail.com>
2020-03-16 10:28:56 +0100

    added client delivery page

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
+
+class Deliveries(http.Controller):
+
+    @http.route('/deliveries/list/', auth='user', website=True, methods=['GET'], type='http')
+    def list(self, **kw):
+        return http.request.render('riders_logistics.deliveries', {
+            'objects': http.request.env['delivery.order'].search([('client_id.user_ids.id', '=', http.request.uid)]),
+        })
+
 # class RidersLogistics(http.Controller):
 #     @http.route('/riders_logistics/riders_logistics/', auth='public')
 #     def index(self, **kw):
